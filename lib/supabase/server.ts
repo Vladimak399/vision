2bad09f64f754a6f4a26a097774092f52a1860b1
Vladimak399 <makin05@yandex.ru>
2026-07-06 08:52:36 +0200

    Support cookie updates in server Supabase client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,15 +1,22 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-import { env } from "../env";
+import { getPublicEnv } from "../env";
 
 export async function createSupabaseServerClient() {
+  const env = getPublicEnv();
   const cookieStore = await cookies();
 
   return createServerClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
     cookies: {
       getAll: () => cookieStore.getAll(),
-      setAll: () => {},
+      setAll: (cookiesToSet) => {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
+        } catch {
+          // Server Components cannot set cookies. Middleware/server actions can.
+        }
+      },
     },
   });
 }
