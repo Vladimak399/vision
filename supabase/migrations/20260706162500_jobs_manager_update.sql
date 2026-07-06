f0811c7970d0b9bda234afbbb0682c49e84d8956
Vladimak399 <makin05@yandex.ru>
2026-07-06 17:47:21 +0200

    fix: allow managers to update jobs

---
+++ supabase/migrations/20260706162500_jobs_manager_update.sql
@@ -0,0 +1,7 @@
+drop policy if exists jobs_manager_update on public.jobs;
+
+create policy jobs_manager_update
+on public.jobs
+for update
+using (public.has_company_role(company_id, array['admin','manager']::public.member_role[]))
+with check (public.has_company_role(company_id, array['admin','manager']::public.member_role[]));
