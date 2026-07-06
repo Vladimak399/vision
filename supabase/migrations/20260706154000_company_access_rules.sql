a32e4e2c8af58369095a7960b62733ffcfb1cbf9
Vladimak399 <makin05@yandex.ru>
2026-07-06 16:45:45 +0200

    fix: align company access rules

---
+++ supabase/migrations/20260706154000_company_access_rules.sql
@@ -0,0 +1,3 @@
+drop policy if exists stores_member_insert on public.stores;
+drop policy if exists competitors_member_insert on public.competitors;
+drop policy if exists catalog_products_insert on public.catalog_products;
